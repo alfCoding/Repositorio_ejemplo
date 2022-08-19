e49b53292d8b94446bdac38f6fb76cae0cc48977
alfCoding <agonza9712@gmail.com>
2022-08-18 20:15:23 -0500

    V3 - Añadir atributos y constructor a Productos

--- src/Productos.java
+++ src/Productos.java
@@ -1,4 +1,15 @@
 
 public class Productos {
     
+    private String nombre;
+    private int preio;
+    private String catergoria;
+
+    public Productos(String nombre, int preio, String catergoria) {
+        this.nombre = nombre;
+        this.preio = preio;
+        this.catergoria = catergoria;
+    }
+    
+    
 }
